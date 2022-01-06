b1d53893c304e25d66fc205626511b6569581596
hydra029 <89290006+hydra029@users.noreply.github.com>
2022-01-06 18:59:25 +0700

    Update delete_order.php

--- delete_order.php
+++ delete_order.php
@@ -4,7 +4,7 @@ $customer_id = $_SESSION['customer_id'];
 $id = $_GET['id'];
 $status = $_GET['status'];
 require 'connect.php';
-$sql = "update receipts set status = '8' where id = '$id' and customer_id = '$customer_id'";
+$sql = "update receipts set status = '7' where id = '$id' and customer_id = '$customer_id'";
 mysqli_query($connect,$sql);
 mysqli_close($connect);
 $_SESSION['success'] = "Xoá đơn hàng thành công";
