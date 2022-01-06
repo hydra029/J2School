3f3ae3696889624f4332eb29a6834d3bb6d517cd
Bùi Hữu Lộc <feature451@gmail.com>
2022-01-06 19:08:42 +0700

    fix manage receipts in admin

--- admin/receipts/update_receipt.php
+++ admin/receipts/update_receipt.php
@@ -5,14 +5,14 @@ $status = $_GET['status'];
 
 require '../connect_database.php';
 
-if ($status == 3) {
-	$sql_command_update = "update receipts set status = 3 where id = $id";
+if ($status == 4) {
+	$sql_command_update = "update receipts set status = 4 where id = $id";
 	mysqli_query($connect_database, $sql_command_update);
 } else if ($status == 5 ) {
 	$sql_command_update = "update receipts set status = 5 where id = $id";
 	mysqli_query($connect_database, $sql_command_update);
-} else if ($status == 6 ) {
-	$sql_command_update = "update receipts set status = 6 where id = $id";
+} else if ($status == 2 ) {
+	$sql_command_update = "update receipts set status = 2 where id = $id";
 	mysqli_query($connect_database, $sql_command_update);
 }
 
