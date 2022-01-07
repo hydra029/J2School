29a0ca383c00658ffa21d7d2c88511acb6ea7c90
Bùi Hữu Lộc <91431461+maoleng@users.noreply.github.com>
2022-01-08 01:02:34 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,4 +1,4 @@
 <?php 
-$connect = mysqli_connect('localhost','feature45','','quan_ly_ban_hang');
+$connect = mysqli_connect('localhost','root','','quan_ly_ban_hang');
 mysqli_set_charset($connect,'utf8');
 ?>
