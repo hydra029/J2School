86bbc3438618a43b230c7847b8b86442b084bd79
Bùi Hữu Lộc <91431461+maoleng@users.noreply.github.com>
2022-01-08 01:02:53 +0700

    Update connect_database.php

--- admin/connect_database.php
+++ admin/connect_database.php
@@ -1,6 +1,6 @@
 <?php 
 
-$connect_database = mysqli_connect('localhost', 'feature45', '', 'quan_ly_ban_hang');
+$connect_database = mysqli_connect('localhost', 'root', '', 'quan_ly_ban_hang');
 mysqli_set_charset($connect_database, 'utf8');
 
 
