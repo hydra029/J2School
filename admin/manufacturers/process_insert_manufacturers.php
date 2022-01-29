592ad0fcca0e52239060db0f5724c43bcc2070d8
Bùi Hữu Lộc <feature451@gmail.com>
2022-01-30 01:28:18 +0700

    new feature: view history in admin site

--- admin/manufacturers/process_insert_manufacturers.php
+++ admin/manufacturers/process_insert_manufacturers.php
@@ -23,10 +23,27 @@ $sql_command_insert = "insert into manufacturers (name, phone, address, image)
 values ('$name', '$phone', '$address', '$image')";
 mysqli_query($connect_database, $sql_command_insert);
 
+//insert vào bảng activity
+$sql_select_last_id_activity = "
+	SELECT id from activities 
+	ORDER BY id DESC
+	LIMIT 1
+";
+$last_activity_id = mysqli_fetch_array(mysqli_query($connect_database, $sql_select_last_id_activity))['id'];
+$activity_id = $last_activity_id + 1;
+$person = $_SESSION['name'];
+$activity_log = "$person đã thêm nhà sản xuất $name" ;
+$sql_insert_activities = "
+	INSERT INTO activities(id, activity)
+	VALUES('$activity_id', '$activity_log')
+";
+
+mysqli_query($connect_database, $sql_insert_activities);
+
+
+//kiểm tra xem có lỗi
 $error = mysqli_error($connect_database);
-
 mysqli_close($connect_database);
-
 if (empty($error)) {
 	$_SESSION['success'] = 'Thêm nhà sản xuất thành công';
 	header('location:index_manufacturers.php');	
