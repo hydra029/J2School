47b18f55cb41a0e76b39c153dedad0f60b961518
Bùi Hữu Lộc <feature451@gmail.com>
2022-02-08 12:31:46 +0700

    format manufacturers manage

--- admin/manufacturers/form_insert_manufacturers.php
+++ admin/manufacturers/form_insert_manufacturers.php
@@ -6,42 +6,47 @@ require '../validate.php';
 <html>
 <head>
 	<title></title>
-	<link rel="stylesheet" href="../index1.css">
+	<link rel="stylesheet" href="../index3.css">
 	<link rel="stylesheet" href="../style_validate1.css">
-</head>  
-<body>
+	<link rel="stylesheet" type="text/css" href="../style_table.css">
+</head>
 
-<div class="all">
-	<div class="left">
-		<?php require '../menu.php'; ?>
-	</div> 
-
-
-	<div class="right">
-		<div class="top">
-
-		</div>
-
-		<div class = "bot">
-
-			<div class = "header">
-				<h1 class =  "header" >Thêm nhà sản xuất</h1>
-			</div>
-			<br>
-
-			<form action = "process_insert_manufacturers.php" method = "post" id = "form-insert-manufactures">
-				Tên nhà sản xuất
-				<input type="text" name="name"><br>
-				Số điện thoại
-				<input type="text" name="phone"><br>
-				Địa chỉ
-				<textarea name = "address"></textarea><br>
-				Hình ảnh
-				<input type="text" name="image"><br>
-				<button>Thêm</button>
-			</form>
+<body> 
+<?php require '../menu.php'; ?>
+<div class="top">
+	<div class = "search">
+		<form class = "form_search">
+			Tìm kiếm
+			<input type="search" name="search" value = "<?php echo $content_search ?>">
+			<button>
+				<img src="../style/style_image/icon_search.png" width="50px">
+			</button>
+		</form>
 	</div>
+
+	<div class = "login">
+		<a class = "login" href="https://google.com">Đăng nhập</a>
+	</div> 
 </div>
+
+<div class = "bot">
+	<div class = "header">
+		<h1 class =  "header" >Thêm nhà sản xuất</h1>
+	</div>
+	<br>
+	<form action = "process_insert_manufacturers.php" method = "post" id = "form-insert-manufactures">
+		Tên nhà sản xuất
+		<input type="text" name="name"><br>
+		Số điện thoại
+		<input type="text" name="phone"><br>
+		Địa chỉ
+		<textarea name = "address"></textarea><br>
+		Hình ảnh
+		<input type="text" name="image"><br>
+		<button>Thêm</button>
+	</form>
+</div>
+
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.3/dist/jquery.validate.min.js"></script>
--- admin/manufacturers/index_manufacturers.php
+++ admin/manufacturers/index_manufacturers.php
@@ -1,17 +1,15 @@
 <?php require '../check_super_admin_login.php'; 
-
 ?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title></title>
-	<link rel="stylesheet" href="../index1.css">
+	<link rel="stylesheet" href="../index3.css">
 	<link rel="stylesheet" href="../style_validate1.css">
+	<link rel="stylesheet" type="text/css" href="../style_table.css">
 </head>
-<body>
 
 <?php require '../connect_database.php';
-
 
 $content_search = '';
 
@@ -25,14 +23,10 @@ if (isset($_GET['page'])) {
 	$index_page = 1 ;
 }
 
-
-
 //lấy ra tổng số nhà sản xuất
 $sql_command_count_manufacturers = "select count(*) from manufacturers where name like '%$content_search%'";
 $query_sql_command_count_manufacturers = mysqli_query($connect_database, $sql_command_count_manufacturers);
 $count_manufacturers = mysqli_fetch_array($query_sql_command_count_manufacturers)['count(*)'];
-
-
 
 //tổng số nhà sản xuất trên 1 trang => 4
 $manufacturers_per_page = 4;
@@ -53,86 +47,65 @@ $query_sql_command_select = mysqli_query($connect_database, $sql_command_select)
 // 	$index_page = 1;
 // }
 
-
 ?>
 
-
-
-
-
- 
-<div class="all">
-	<div class="left">
-		<?php require '../menu.php'; ?>
-	</div> 
-
-
-	<div class="right">
-		<div class="top">
-
-			<div class = "search">
-				<form class = "form_search">
-					Tìm kiếm
-					<input type="search" name="search" value = "<?php echo $content_search ?>">
-					<button>
-						<img src="../style/style_image/icon_search.png" width="50px">
-					</button>
-				</form>
-			</div>
-
-			<div class = "login">
-				<a class = "login" href="https://google.com">Đăng nhập</a>
-			</div> 
-		</div>
-
-
-		<div class = "bot">
-
-			<div class = "header">
-				<h1 class =  "header" >NHÀ SẢN XUẤT</h1>
-			</div>
-			<br>
-			<?php require '../validate.php' ?>
-			<table class="table">
-				<tr>
-					<th>Mã</th>
-					<th>Tên nhà sản xuất</th>
-					<th>Số điện thoại</th>
-					<th>Địa chỉ</th>
-					<th>Hình ảnh</th>
-					<th>Sửa</th>
-					<th>Xóa</th>
-				</tr>
-				<?php foreach ($query_sql_command_select as $array_manufacturers): ?>
-				<tr>
-					<td><?php echo $array_manufacturers['id'] ?></td>
-					<td><?php echo $array_manufacturers['name'] ?></td>
-					<td><?php echo $array_manufacturers['phone'] ?></td>
-					<td><?php echo $array_manufacturers['address'] ?></td>
-					<td><img src="<?php echo $array_manufacturers['image'] ?>" height = "100px"></td>
-					<td>
-						<a href="form_update_manufacturers.php?id=<?php echo $array_manufacturers['id'] ?>">Sửa</a>
-					</td>
-					<td>
-						<a href="process_delete_manufacturers.php?id=<?php echo $array_manufacturers['id'] ?>">Xóa</a>
-					</td>
-				</tr>
-				<?php endforeach ?>
-			</table>
-		<?php for ($index_page = 1; $index_page <= $count_pages; $index_page++) { ?>
-			<a href="?page=<?php echo $index_page?>&search=<?php $content_search ?>">
-				<?php echo $index_page ?>
-			</a>
-		
-		<?php } ?>
-
-
-		</div>
-
+<body> 
+<?php require '../menu.php'; ?>
+<div class="top">
+	<div class = "search">
+		<form class = "form_search">
+			Tìm kiếm
+			<input type="search" name="search" value = "<?php echo $content_search ?>">
+			<button>
+				<img src="../style/style_image/icon_search.png" width="50px">
+			</button>
+		</form>
 	</div>
 
+	<div class = "login">
+		<a class = "login" href="https://google.com">Đăng nhập</a>
+	</div> 
 </div>
 
+<div class = "bot">
+	<div class = "header">
+		<h1 class =  "header" >NHÀ SẢN XUẤT</h1>
+	</div>
+	<br>
+	<?php require '../validate.php' ?>
+	<table>
+		<tr>
+			<th>Mã</th>
+			<th>Tên nhà sản xuất</th>
+			<th>Số điện thoại</th>
+			<th>Địa chỉ</th>
+			<th>Hình ảnh</th>
+			<th>Sửa</th>
+			<th>Xóa</th>
+		</tr>
+		<?php foreach ($query_sql_command_select as $array_manufacturers): ?>
+			<tr>
+				<td><?php echo $array_manufacturers['id'] ?></td>
+				<td><?php echo $array_manufacturers['name'] ?></td>
+				<td><?php echo $array_manufacturers['phone'] ?></td>
+				<td><?php echo $array_manufacturers['address'] ?></td>
+				<td><img src="<?php echo $array_manufacturers['image'] ?>" height = "100px"></td>
+				<td>
+					<a href="form_update_manufacturers.php?id=<?php echo $array_manufacturers['id'] ?>">Sửa</a>
+				</td>
+				<td>
+					<a href="process_delete_manufacturers.php?id=<?php echo $array_manufacturers['id'] ?>">Xóa</a>
+				</td>
+			</tr>
+		<?php endforeach ?>
+	</table>
+	<?php for ($index_page = 1; $index_page <= $count_pages; $index_page++) { ?>
+		<a href="?page=<?php echo $index_page?>&search=<?php $content_search ?>">
+			<?php echo $index_page ?>
+		</a>
 
+	<?php } ?>
+	
+</div>
 </body>
 </html>
