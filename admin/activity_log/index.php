8a58084efa453db3d672146426338654574b1735
Bùi Hữu Lộc <feature451@gmail.com>
2022-02-08 12:28:54 +0700

    format: view history

--- admin/activity_log/index.php
+++ admin/activity_log/index.php
@@ -5,10 +5,11 @@
 <html>
 <head>
 	<title></title>
-	<link rel="stylesheet" href="../index1.css">
+	<link rel="stylesheet" href="../index3.css">
 	<link rel="stylesheet" href="../style_validate1.css">
+	<link rel="stylesheet" type="text/css" href="../style_table.css">
 </head>
-<body>
+
 
 <?php require '../connect_database.php';
 if ( empty($_GET['page']) ) {
@@ -34,58 +35,49 @@ $sql_select_activity = "
 $query_sql_select_activity = mysqli_query($connect_database, $sql_select_activity);
 
 ?>
-
-
-<div class="all">
-	<div class="left">
-		<?php require '../menu.php'; ?>
-	</div> 
-
-	<div class="right">
-		<div class="top">
-			<div class = "search">
-				<form class = "form_search">
-					Tìm kiếm
-					<input type="search" name="search" value = "<?php echo $content_search ?>">
-					<button>
-						<img src="../style/style_image/icon_search.png" width="50px">
-					</button>
-				</form>
-			</div>
-
-			<div class = "login">
-				<a class = "login" href="https://google.com">Đăng nhập</a>
-			</div> 
-		</div>
-
-
-		<div class = "bot">
-
-			<div class = "header">
-				<h1 class =  "header" >LỊCH SỬ HOẠT ĐỘNG</h1>
-			</div>
-			<br>
-			<?php require '../validate.php' ?>
-			<table class="table">
-				<tr>
-					<th>Mã</th>
-					<th>Hoạt động</th>
-					<th>Vào lúc</th>
-				</tr>
-				<?php foreach ($query_sql_select_activity as $each_activity): ?>
-				<tr>
-					<td><?php echo $each_activity['id'] ?></td>
-					<td><?php echo $each_activity['activity'] ?></td>
-					<td><?php echo $each_activity['time'] ?></td>
-				</tr>
-				<?php endforeach ?>
-			</table>
-			<?php for ($i=1; $i <= $pages; $i++) {  ?>
-				<a href = "?page=<?php echo $i ?>"><?php echo $i ?></a>
-			<?php } ?>
-		</div>
-
+	
+<body> 
+<?php require '../menu.php'; ?>
+<div class="top">
+	<div class = "search">
+		<form class = "form_search">
+			Tìm kiếm
+			<input type="search" name="search" value = "<?php echo $content_search ?>">
+			<button>
+				<img src="../style/style_image/icon_search.png" width="50px">
+			</button>
+		</form>
 	</div>
+
+	<div class = "login">
+		<a class = "login" href="https://google.com">Đăng nhập</a>
+	</div> 
+</div>
+
+<div class = "bot">
+	<div class = "header">
+		<h1 class =  "header" >LỊCH SỬ HOẠT ĐỘNG</h1>
+	</div>
+	<br>
+	<?php require '../validate.php' ?>
+	<table class="table">
+		<tr>
+			<th>Mã</th>
+			<th>Hoạt động</th>
+			<th>Vào lúc</th>
+		</tr>
+		<?php foreach ($query_sql_select_activity as $each_activity): ?>
+		<tr>
+			<td><?php echo $each_activity['id'] ?></td>
+			<td><?php echo $each_activity['activity'] ?></td>
+			<td><?php echo $each_activity['time'] ?></td>
+		</tr>
+		<?php endforeach ?>
+	</table>
+	<?php for ($i=1; $i <= $pages; $i++) {  ?>
+		<a href = "?page=<?php echo $i ?>"><?php echo $i ?></a>
+	<?php } ?>
+
 
 </div>
 
