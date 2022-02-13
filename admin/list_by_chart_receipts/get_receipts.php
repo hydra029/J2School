2cbef600fa1f7725ed57ac26cae1707790c1f0e3
Bùi Hữu Lộc <feature451@gmail.com>
2022-02-13 19:46:27 +0700

    complete chart receipts

--- admin/list_by_chart_receipts/get_receipts.php
+++ admin/list_by_chart_receipts/get_receipts.php
@@ -1,14 +1,40 @@
 <?php 
 
-$max_day_of_this_month_to_get = $_GET['days'];
+if ( isset($_GET['days']) ) {
+	$max_day_of_this_month_to_get = $_GET['days'];
+	$sql_command_select = "
+		SELECT status, round((count(*) * 100.0 / sum(count(status)) over()), 2) as 'percent'
+		FROM receipts 
+		WHERE DATE(order_time) >= (CURDATE() - INTERVAL '$max_day_of_this_month_to_get' DAY)	
+		GROUP BY receipts.status;
+	"; 
+}
+
+if ( isset($_GET['day_to_day_1'], $_GET['day_to_day_2']) ) {	
+	$day_to_day_1 = $_GET['day_to_day_1'];
+	$day_to_day_2 = $_GET['day_to_day_2'];
+	$sql_command_select = "
+		SELECT status, round((count(*) * 100.0 / sum(count(status)) over()), 2) as 'percent'
+		FROM receipts 
+		WHERE order_time >= '$day_to_day_1' and order_time <= '$day_to_day_2'
+		GROUP BY receipts.status;
+	";
+}
+
+if ( isset($_GET['month']) ) {
+	$month = $_GET['month'];
+	$max_day_of_month = date("t", strtotime($month));
+	$day_1 = $month . '-01';
+	$day_2 = $month . '-' . $max_day_of_month;
+	$sql_command_select = "
+		SELECT status, round((count(*) * 100.0 / sum(count(status)) over()), 2) as 'percent'
+		FROM receipts 
+		WHERE order_time >= '$day_1' and order_time <= '$day_2'
+		GROUP BY receipts.status;
+	"; 
+}
 
 require '../connect_database.php';
-$sql_command_select = "
-	SELECT status, round((count(*) * 100.0 / sum(count(status)) over()), 2) as 'percent'
-	FROM receipts 
-	WHERE DATE(order_time) >= (CURDATE() - INTERVAL '$max_day_of_this_month_to_get' DAY)	
-	GROUP BY receipts.status;
-";
 
 $query_sql_command_select = mysqli_query($connect_database, $sql_command_select);
 $array = [];
--- admin/list_by_chart_receipts/index.php
+++ admin/list_by_chart_receipts/index.php
@@ -29,7 +29,22 @@
 	<div class = "header">
 		<h1 class =  "header" >THỐNG KÊ HÓA ĐƠN THEO TRẠNG THÁI</h1>
 	</div>
-	<input type="date" name="">
+	Thống kê theo
+	<select>
+		<option disabled selected>Chọn cách thống kê</option>
+		<option class="option_day_to_day" value = "1">Ngày chỉ định tới ngày chỉ định</option>
+		<option class="option_days_ago" value="2">Số ngày gần đây (tối đa 30)</option>
+		<option class="option_month" value="3">Tháng</option>
+	</select>
+	<br>
+	<form>
+		<input type="date"  id = "input_day_to_day_1" hidden>
+		<br>
+		<input type="date"  id = "input_day_to_day_2" hidden>
+		<p id = "tips" hidden>Chọn xong rồi click chuột ra ngoài để thấy biểu đồ</p>
+		<input type="number"  id = "input_days_ago" hidden>
+		<input type="month"  id = "input_month" hidden>
+	</form>
 	
 	<figure class="highcharts-figure">
 		<div id="container"></div>
@@ -46,57 +61,130 @@
 <script src="https://code.highcharts.com/modules/export-data.js"></script>
 <script src="https://code.highcharts.com/modules/accessibility.js"></script>
 <script type="text/javascript">
-const days = 31
-$(document).ready(function() {
-	$.ajax({
-		url: 'get_receipts.php',
-		type: 'get',
-		dataType: 'json',
-		data: {days},
+	$(document).ready(function() {
+		$("form").keypress(function(event) {
+			if ( event.keyCode === 13 ) {
+				event.preventDefault()
+			}
+		})
+			$('select').on('change', function (e) {
+			var optionSelected = $("option:selected", this);
+			var valueSelected = this.value;
+			switch ( valueSelected ) {
+				case "1":
+				$("#input_day_to_day_1").show()
+				$("#input_day_to_day_2").show()
+				$("#tips").hide()
+				$("#input_days_ago").hide()
+				$("#input_month").hide()
+				break
+				case "2":
+				$("#input_days_ago").show()
+				$("#tips").show()
+				$("#input_day_to_day_1").hide()
+				$("#input_day_to_day_2").hide()
+				$("#input_month").hide()
+				break
+				case "3":
+				$("#input_month").show()
+				$("#input_days_ago").hide()
+				$("#tips").hide()
+				$("#input_day_to_day_1").hide()
+				$("#input_day_to_day_2").hide()
+				break	
+			}
+
+			$("#input_month, #input_days_ago, #input_day_to_day_1, #input_day_to_day_2").change(function(event) {
+				switch ( valueSelected ) {
+					case "1":
+					var day_to_day_1 = $("#input_day_to_day_1").val()
+					var day_to_day_2 = $("#input_day_to_day_2").val()
+					var header_chart = " từ " + day_to_day_1 + " đến " + day_to_day_2
+					$.ajax({
+						url: 'get_receipts.php',
+						type: 'get',
+						dataType: 'json',
+						data: {day_to_day_1, day_to_day_2},
+					})
+					.done(function(response) {
+						const array = Object.values(response)
+						get_chart(array, days)
+					})
+					break
+
+					case "2":
+					var days = $("#input_days_ago").val()
+					var header_chart = " trong " + days + " ngày gần đây"
+					$.ajax({
+						url: 'get_receipts.php',
+						type: 'get',
+						dataType: 'json',
+						data: {days},
+					})
+					.done(function(response) {
+						const array = Object.values(response)
+						get_chart(array, days)
+					})
+					break
+
+					case "3":
+					var month = $("#input_month").val()
+					var header_chart = " trong tháng " + month
+					$.ajax({
+						url: 'get_receipts.php',
+						type: 'get',
+						dataType: 'json',
+						data: {month},
+					})
+					.done(function(response) {
+						const array = Object.values(response)
+						get_chart(array, days)
+					})
+					break
+
+				}
+				
+				function get_chart(array, days) {
+					Highcharts.chart('container', {
+						chart: {
+							plotBackgroundColor: null,
+							plotBorderWidth: null,
+							plotShadow: false,
+							type: 'pie'
+						},
+						title: {
+							text: 'Thống kê hóa đơn theo trạng thái ' + header_chart
+						},
+						tooltip: {
+							pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+						},
+						accessibility: {
+							point: {
+								valueSuffix: '%'
+							}
+						},
+						plotOptions: {
+							pie: {
+								allowPointSelect: true,
+								cursor: 'pointer',
+								dataLabels: {
+									enabled: true,
+									format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+								}
+							}
+						},
+						series: [{
+							name: 'Brands',
+							colorByPoint: true,
+							data: array
+						}]
+					});
+				}
+				
+			})
+		})
+
 	})
-	.done(function(response) {
-		const array = Object.values(response)
-		get_chart(array, days)
-	})
-		
-function get_chart(array, days) {
-	Highcharts.chart('container', {
-	  chart: {
-	    plotBackgroundColor: null,
-	    plotBorderWidth: null,
-	    plotShadow: false,
-	    type: 'pie'
-	  },
-	  title: {
-	    text: 'Thống kê hóa đơn theo trạng thái trong ' + days + ' gần đây'
-	  },
-	  tooltip: {
-	    pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-	  },
-	  accessibility: {
-	    point: {
-	      valueSuffix: '%'
-	    }
-	  },
-	  plotOptions: {
-	    pie: {
-	      allowPointSelect: true,
-	      cursor: 'pointer',
-	      dataLabels: {
-	        enabled: true,
-	        format: '<b>{point.name}</b>: {point.percentage:.1f} %'
-	      }
-	    }
-	  },
-	  series: [{
-	    name: 'Brands',
-	    colorByPoint: true,
-	    data: array
-	  }]
-	});
-}
-	
-})
 </script>
 </body>
 </html>
