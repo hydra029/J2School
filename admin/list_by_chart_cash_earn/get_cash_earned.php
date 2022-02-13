28f7b2a739079cd80e4553fb20b02d8710e01da2
Bùi Hữu Lộc <feature451@gmail.com>
2022-02-13 21:39:52 +0700

    complete chart: cash earned

--- admin/list_by_chart_cash_earn/get_cash_earned.php
+++ admin/list_by_chart_cash_earn/get_cash_earned.php
@@ -1,44 +1,90 @@
 <?php 
 
-$max_day_of_this_month_to_get = $_GET['days'];
+if ( isset($_GET['days']) ) {
+	$max_day_of_this_month_to_get = $_GET['days'];
+	$sql_command_select = "
+		SELECT sum(total) as 'cash_each_day', DATE_FORMAT(order_time, '%e-%m') as 'day'
+		FROM receipts
+		WHERE DATE(order_time) >= (CURDATE() - INTERVAL '$max_day_of_this_month_to_get' DAY)
+		GROUP BY DATE_FORMAT(order_time, '%d-%m')
+	";
+	require '../connect_database.php';
+	$query_sql_command_select = mysqli_query($connect_database, $sql_command_select);
+	$array = [];
 
-require '../connect_database.php';
-$sql_command_select = "
-	SELECT sum(total) as 'cash_each_day', DATE_FORMAT(order_time, '%e-%m') as 'day'
-	FROM receipts
-	WHERE DATE(order_time) >= (CURDATE() - INTERVAL '$max_day_of_this_month_to_get' DAY)
-	GROUP BY DATE_FORMAT(order_time, '%d-%m')
-";
+	$day_today = date('d');
+	$this_month = date('m');
+	if ( $max_day_of_this_month_to_get > $day_today ) {
+		$get_days_last_month = $max_day_of_this_month_to_get - $day_today;
+		$previous_month = date("m",strtotime("-1 month"));
+		$max_day_of_previous_month = date("t", strtotime($previous_month));
+		$start_day_of_last_month = $max_day_of_previous_month - $get_days_last_month;	
 
-$query_sql_command_select = mysqli_query($connect_database, $sql_command_select);
-$array = [];
+		for ( $i = $start_day_of_last_month; $i <= $max_day_of_previous_month; $i++ ) {
+			$index = $i . '-' . $previous_month;
+			$array[$index] = 0;
+		}
+		$start_day_of_this_month = 1;
+	} else {
+		$start_day_of_this_month = $day_today - $max_day_of_this_month_to_get;
+	}
 
-$day_today = date('d');
-$this_month = date('m');
-
-if ( $max_day_of_this_month_to_get > $day_today ) {
-	$get_days_last_month = $max_day_of_this_month_to_get - $day_today;
-	$previous_month = date("m",strtotime("-1 month"));
-	$max_day_of_previous_month = date("t", strtotime($previous_month));
-	$start_day_of_last_month = $max_day_of_previous_month - $get_days_last_month;	
-
-	for ( $i = $start_day_of_last_month; $i <= $max_day_of_previous_month; $i++ ) {
-		$index = $i . '-' . $previous_month;
+	for ( $i = $start_day_of_this_month; $i <= $day_today; $i++ ) {
+		$index = $i . '-' . $this_month;
 		$array[$index] = 0;
 	}
-	$start_day_of_this_month = 1;
-} else {
-	$start_day_of_this_month = $day_today - $max_day_of_this_month_to_get;
+	foreach ($query_sql_command_select as $array_cash_each_day ) {
+		$array[$array_cash_each_day['day']] = (float)$array_cash_each_day['cash_each_day'];
+	}
+
 }
 
+if ( isset($_GET['year']) ) {	
+	$year = $_GET['year'];
+	$sql_command_select = "
+		SELECT sum(total) as 'cash_each_day',month(order_time) as 'month'
+		FROM receipts
+		WHERE year(order_time) = '$year'
+		GROUP BY month(order_time)
+	";
 
+	require '../connect_database.php';
+	$query_sql_command_select = mysqli_query($connect_database, $sql_command_select);
+	$array = [];
 
-for ( $i = $start_day_of_this_month; $i <= $day_today; $i++ ) {
-	$index = $i . '-' . $this_month;
-	$array[$index] = 0;
+	for ( $i = 1; $i <= 12; $i++ ) {
+		$index = $i . '-' . $year;
+		$array[$index] = 0;
+	}
+	foreach ($query_sql_command_select as $array_cash_each_day ) {
+		$array[$array_cash_each_day['month']] = (float)$array_cash_each_day['cash_each_day'];
+	}
 }
-foreach ($query_sql_command_select as $array_cash_each_day ) {
-	$array[$array_cash_each_day['day']] = (float)$array_cash_each_day['cash_each_day'];
+
+if ( isset($_GET['month']) ) {
+	$month = $_GET['month'];
+	$max_day_of_this_month_to_get = date("t", strtotime($month));
+	$that_month = date("m", strtotime($month));
+	$day_1 = $month . '-01';
+	$day_2 = $month . '-' . $max_day_of_this_month_to_get;
+	$sql_command_select = "
+		SELECT sum(total) as 'cash_each_day', DATE_FORMAT(order_time, '%e-%m') as 'day'
+		FROM receipts
+		WHERE order_time >= '$day_1' and order_time <= '$day_2'
+		GROUP BY DATE_FORMAT(order_time, '%d-%m')
+	";
+
+	require '../connect_database.php';
+	$query_sql_command_select = mysqli_query($connect_database, $sql_command_select);
+	$array = [];
+
+	for ( $i = 1; $i <= $max_day_of_this_month_to_get; $i++ ) {
+		$index = $i . '-' . $that_month;
+		$array[$index] = 0;
+	}
+	foreach ($query_sql_command_select as $array_cash_each_day ) {
+		$array[$array_cash_each_day['day']] = (float)$array_cash_each_day['cash_each_day'];
+	}
 }
 
 echo json_encode($array);
--- admin/list_by_chart_cash_earn/index.php
+++ admin/list_by_chart_cash_earn/index.php
@@ -41,7 +41,28 @@
 	</div>
 	<br>
 	<?php require '../validate.php' ?>
-	<input type="date" name="">
+	
+	Thống kê theo
+	<select>
+		<option disabled selected>Chọn cách thống kê</option>
+		<option class="option_year" value = "1">Thống kê theo năm</option>
+		<option class="option_days_ago" value="2">Số ngày gần đây (tối đa 30)</option>
+		<option class="option_month" value="3">Tháng</option>
+	</select>
+	<br>
+	<form>
+		<input type="date"  id = "input_day_to_day_1" hidden>
+		<br>
+		<select id="input_year" hidden>
+			<option disabled selected>Chọn năm</option>
+			<?php for ( $i = date("Y"); $i > date("Y") - 5; $i-- ) { ?>
+			<option><?php echo $i ?></option>
+			<?php } ?>
+		</select>
+		<input type="number"  id = "input_days_ago" hidden>
+		<input type="month"  id = "input_month" hidden>
+	</form>
+	
 	<figure class="highcharts-figure">
 		<div id="container"></div>
 	</figure>
@@ -56,71 +77,146 @@
 <script src="https://code.highcharts.com/modules/accessibility.js"></script>
 <script src="https://code.highcharts.com/modules/series-label.js"></script>
 <script type="text/javascript">
-const days = 31
+
 $(document).ready(function() {
-	$.ajax({
-		url: 'get_cash_earned.php',
-		type: 'get',
-		dataType: 'json',
-		data: {days},
-	})
-	.done(function(response) {
-		var arrayX = Object.keys(response)
-		var arrayY = Object.values(response)
-		Highcharts.chart('container', {
+$("form").keypress(function(event) {
+	if ( event.keyCode === 13 ) {
+		event.preventDefault()
+	}
+})
+$('select').on('change', function (e) {
+    var optionSelected = $("option:selected", this);
+    var valueSelected = this.value;
+    switch ( valueSelected ) {
+    	case "1":
+    		$("#input_year").show()
+    		$("#tips").hide()
+    		$("#input_days_ago").hide()
+    		$("#input_month").hide()
+    		break
+    	case "2":
+    		$("#input_days_ago").show()
+    		$("#tips").show()
+    		$("#input_year").hide()
+    		$("#input_month").hide()
+    		break
+    	case "3":
+			$("#input_month").show()
+    		$("#input_days_ago").hide()
+    		$("#tips").hide()
+    		$("#input_year").hide()
+    		break	
+    }
 
-		  title: {
-		    text: 'Thống kê doanh thu ' + days + ' ngày gần nhất'
-		  },
-
-		  yAxis: {
-		    title: {
-		      text: 'Doanh thu'
-		    }
-		  },
-
-		  xAxis: {
-	        categories: arrayY
-		  },
-
-		  legend: {
-		    layout: 'vertical',
-		    align: 'right',
-		    verticalAlign: 'middle'
-		  },
-
-		  plotOptions: {
-		    series: {
-		      label: {
-		        connectorAllowed: false
-		      },
-		    }
-		  },
-
-		  series: [{
-		    name: 'Doanh thu',
-		    data: arrayY
-		  }],
-
-		  responsive: {
-		    rules: [{
-		      condition: {
-		        maxWidth: 500
-		      },
-		      chartOptions: {
-		        legend: {
-		          layout: 'horizontal',
-		          align: 'center',
-		          verticalAlign: 'bottom'
-		        }
-		      }
-		    }]
-		  }
-
-		});
+$("#input_month, #input_days_ago, #input_year").change(function(event) {
+	switch ( valueSelected ) {
+		case "1":
+		var year = $("#input_year").val()
+		var header_chart = " theo năm " + year
+		$.ajax({
+			url: 'get_cash_earned.php',
+			type: 'get',
+			dataType: 'json',
+			data: {year},
 		})
-	
-})	
+		.done(function(response) {
+			var arrayX = Object.keys(response)
+			var arrayY = Object.values(response)
+			get_chart(arrayX, arrayY)
+		})
+		break
+
+		case "2":
+		var days = $("#input_days_ago").val()
+		var header_chart = " trong " + days + " ngày gần đây"
+		$.ajax({
+			url: 'get_cash_earned.php',
+			type: 'get',
+			dataType: 'json',
+			data: {days},
+		})
+		.done(function(response) {
+			var arrayX = Object.keys(response)
+			var arrayY = Object.values(response)
+			get_chart(arrayX, arrayY)
+		})
+		break
+
+		case "3":
+		var month = $("#input_month").val()
+		var header_chart = " trong tháng " + month
+		$.ajax({
+			url: 'get_cash_earned.php',
+			type: 'get',
+			dataType: 'json',
+			data: {month},
+		})
+		.done(function(response) {
+			var arrayX = Object.keys(response)
+			var arrayY = Object.values(response)
+			get_chart(arrayX, arrayY)
+		})
+		break
+	}
+
+	function get_chart(arrayX, arrayY) {
+	Highcharts.chart('container', {
+
+		title: {
+			text: 'Thống kê doanh thu ' + header_chart
+		},
+
+		yAxis: {
+			title: {
+				text: 'Doanh thu'
+			}
+		},
+
+		xAxis: {
+			categories: arrayX
+		},
+
+		legend: {
+			layout: 'vertical',
+			align: 'right',
+			verticalAlign: 'middle'
+		},
+
+		plotOptions: {
+			series: {
+				label: {
+					connectorAllowed: false
+				},
+			}
+		},
+
+		series: [{
+			name: 'Doanh thu',
+			data: arrayY
+		}],
+
+		responsive: {
+			rules: [{
+				condition: {
+					maxWidth: 500
+				},
+				chartOptions: {
+					legend: {
+						layout: 'horizontal',
+						align: 'center',
+						verticalAlign: 'bottom'
+					}
+				}
+			}]
+		}
+
+	})
+}
+
+})
+
+})
+})
 </script>
 
 </body>
